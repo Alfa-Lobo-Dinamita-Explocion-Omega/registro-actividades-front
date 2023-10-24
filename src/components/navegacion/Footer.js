45f5ae1d17be79c9d11c3e924659df73aa0333ae
alejandrouribea <alejandro.uribea@udea.edu.co>
2023-10-23 20:59:01 -0500

    estructura base

---
+++ src/components/navegacion/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import footer from '../../assets/footer.png'; 
+
+
+function Footer() {
+    return (
+    <div>
+        <img src={footer} className="footer"/>
+    </div>
+  )
+}
+
+export default Footer
